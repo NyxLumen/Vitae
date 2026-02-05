7e77689ef0ec2aa629e99ceb09c2bee1d1b59509
Aditya Sharma <ads.as2020@gmail.com>
2026-02-05 17:22:06 +0530

    created folder struct and implemented basic web page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
 import { useState } from "react";
-import "./App.css";
+import styles from "./App.module.css";
+
+const INITIAL_DATA = {
+	general: {
+		fullName: "John Doe",
+		email: "john@example.com",
+		phone: "+1 555-0123",
+		location: "New York, NY",
+	},
+	education: [],
+	experience: [],
+};
 
 function App() {
+	const [resumeData, setResumeData] = useState(INITIAL_DATA);
+
+	const [template, setTemplate] = useState("harvard");
+
 	return (
-		<div className="app">
-			<h1>Vitae</h1>
+		<div className={styles.appContainer}>
+			<div className={styles.editorSection}>
+				<h1>Vitae Editor</h1>
+			</div>
+
+			<div className={styles.previewSection}>
+				{template === "harvard" && (
+					<div className="template-harvard">Harvard View (Coming Soon)</div>
+				)}
+				{template === "modern" && (
+					<div className="template-modern">Modern View (Coming Soon)</div>
+				)}
+			</div>
 		</div>
 	);
 }
---
+++ src/components/editor/Education.jsx
@@ -0,0 +1,36 @@
+export default function Education() {
+	return (
+		<>
+			<h1>Hello Nigga</h1>
+			<img src="" alt="" />
+			<p>what do you intend to do</p>
+			<h2>still curios tho</h2>
+			<p>
+				is this what you really want
+				<p>
+					actully?
+					<p>i think you might be dreaming</p>
+				</p>
+				<img src="" alt="" />
+				<h3>what do you want really</h3>
+				<div>
+					<div>
+						<h2>heading</h2>
+						<p>
+							what is there to talk about in this world if not domain expansion
+						</p>
+						<span>what is love</span>
+						<h3>is that you want</h3>
+					</div>
+					<h2>heading</h2>
+					<p>
+						what is there to talk about in this world if not domain expansion
+					</p>
+					<span>what is love</span>
+					<h3>is that you want</h3>
+					<p>if that is what u want un</p>
+				</div>
+			</p>
+		</>
+	);
+}
